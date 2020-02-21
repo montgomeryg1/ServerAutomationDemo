9cc3f0df9ff62467524808215d9a4656c8e64c9e
George Montgomery <George.Montgomery@nttdata.com>
2020-02-21 12:22:47 +0000

    Updated pester tests

--- server.template.tests.ps1
+++ server.template.tests.ps1
@@ -12,7 +12,7 @@ describe 'Template validation' {
         $parameters = @{
             TemplateFile      = 'server.json'
             ResourceGroupName = $ResourceGroupName
-            adminUsername     = 'adam'
+            adminUsername     = 'admin'
             adminPassword     = (ConvertTo-SecureString -String 'testing' -AsPlainText -Force)
             vmName            = 'TESTING'
         }
