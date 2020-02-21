919e707945885d16dc805a17bea2d5e6a514cde1
George Montgomery <George.Montgomery@nttdata.com>
2020-02-21 12:47:50 +0000

    Updated template validation

--- server.template.tests.ps1
+++ server.template.tests.ps1
@@ -7,7 +7,29 @@ param(
 
 #requires -Module Az.Accounts,Az.Resources
 
+$templateARM = Get-Content 'server.json' -Raw -ErrorAction SilentlyContinue
+$template = ConvertFrom-Json -InputObject $templateARM -ErrorAction SilentlyContinue
+
 describe 'Template validation' {
+
+    It 'Template ARM File Exists' {
+        Test-Path 'server.json' -Include '*.json' | Should Be $true
+    }
+
+    It 'Is a valid JSON file' {
+        $templateARM | ConvertFrom-Json -ErrorAction SilentlyContinue | Should Not Be $Null
+    }
+
+    It "Contains all required elements" {
+        $Elements = '$schema',
+                    'contentVersion',
+                    'outputs',
+                    'parameters',
+                    'resources'                                
+          $templateProperties = $template | Get-Member -MemberType NoteProperty | % Name
+          $templateProperties | Should Be $Elements
+    }
+
     it 'template passes validation check' {
         $parameters = @{
             TemplateFile      = 'server.json'
@@ -18,4 +40,7 @@ describe 'Template validation' {
         }
         (Test-AzResourceGroupDeployment @parameters).Details | should -Benullorempty
     }
+
+
+    
 }
